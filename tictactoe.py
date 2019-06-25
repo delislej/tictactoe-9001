1b5566222de94818268fe49e563b03cb3b832769
joseph delisle <delislej@gmail.com>
2019-06-24 22:47:43 -0700

    BIG UPDATE!
    finished game status code, finished win checking, finished reset, added reset button

--- tictactoe.py
+++ tictactoe.py
@@ -8,16 +8,13 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from functools import partial
+import time
+
 
 class Ui_Dialog(object):
-    board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    board = [-1, -2, -3, -4, -5, -6, -7, -8, -9]
     turn = 0
     status = 0
-
-    def __init__(self):
-        board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        turn = 1
-        status = 0
 
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
@@ -45,6 +42,10 @@ class Ui_Dialog(object):
         self.pushButton_9.setGeometry(QtCore.QRect(400, 338, 75, 61))
         self.pushButton_9.setText("")
         self.pushButton_9.setObjectName("pushButton_9")
+        self.resetButton = QtWidgets.QPushButton(Dialog)
+        self.resetButton.setGeometry(QtCore.QRect(400, 40, 75, 30))
+        self.resetButton.setText("reset")
+        self.resetButton.setObjectName("reset_button")
         self.pushButton_3 = QtWidgets.QPushButton(Dialog)
         self.pushButton_3.setGeometry(QtCore.QRect(400, 108, 75, 61))
         self.pushButton_3.setText("")
@@ -77,22 +78,27 @@ class Ui_Dialog(object):
         self.pushButton_7.clicked.connect(partial(self.playTurn, self.turn, self.pushButton_7, 6))
         self.pushButton_8.clicked.connect(partial(self.playTurn, self.turn, self.pushButton_8, 7))
         self.pushButton_9.clicked.connect(partial(self.playTurn, self.turn, self.pushButton_9, 8))
+        self.resetButton.clicked.connect(self.reset)
 
     def playTurn(self, turn, button, boardnum):
-
-        if self.turn == 1:
-            # self.comTurn()
-            print("com plays")
-            button.setStyleSheet("background-color: red")
-            self.turn = 0
-            self.lineEdit.setText("turn: " + str(self.turn))
-        else:
-            self.turn = 1
-            button.setStyleSheet("background-color: blue")
-            self.lineEdit.setText("turn: " + str(self.turn))
-
-
-
+        if self.status != 1:
+            if self.turn == 1:
+                # self.comTurn()
+                print("com plays")
+                button.setStyleSheet("background-color: red")
+                self.turn = 0
+                self.lineEdit.setText("turn: " + str(self.turn))
+                self.board[boardnum] = 1
+                self.checkWin()
+            else:
+                if self.board[boardnum] == 1 or self.board[boardnum] == 0:
+                    print("already placed!")
+                else:
+                    self.board[boardnum] = turn
+                    button.setStyleSheet("background-color: blue")
+                    self.turn = 1
+                    self.lineEdit.setText("turn: " + str(self.turn))
+                    self.checkWin()
 
 
     def retranslateUi(self, Dialog):
@@ -101,17 +107,49 @@ class Ui_Dialog(object):
         self.lineEdit.setText(_translate("Dialog", "Turn: 0"))
         self.lineEdit_2.setText(_translate("Dialog", "status"))
 
-    def red(self):
-        self.pushButton.setStyleSheet("background-color: red")
+    def checkWin(self):
+        if (self.board[0] == self.board[1] == self.board[2]) or \
+                (self.board[3] == self.board[4] == self.board[5]) or \
+                (self.board[6] == self.board[7] == self.board[8]) or \
+                (self.board[0] == self.board[3] == self.board[6]) or \
+                (self.board[1] == self.board[4] == self.board[7]) or \
+                (self.board[2] == self.board[5] == self.board[8]) or \
+                (self.board[0] == self.board[4] == self.board[8]) or \
+                (self.board[2] == self.board[4] == self.board[6]) or \
+                (self.board[0] == self.board[1] == self.board[2]):
+            self.lineEdit_2.setText(str(self.turn) + " wins!")
+            self.status = 1
+        count = 0
+        for i in self.board:
+            if i == 1 or i == 0:
+                count += 1
+        if count == 9:
+            self.status = 1
+            self.lineEdit_2.setText("Cats game!")
 
-    def blue(self):
-        self.pushButton_2.setStyleSheet("background-color: blue")
 
 
+    def reset(self):
+
+        self.board = [-1, -2, -3, -4, -5, -6, -7, -8, -9]
+        self.turn = 0
+        self.status = 0
+        self.pushButton.setStyleSheet("background-color: none")
+        self.pushButton_2.setStyleSheet("background-color: none")
+        self.pushButton_3.setStyleSheet("background-color: none")
+        self.pushButton_4.setStyleSheet("background-color: none")
+        self.pushButton_5.setStyleSheet("background-color: none")
+        self.pushButton_6.setStyleSheet("background-color: none")
+        self.pushButton_7.setStyleSheet("background-color: none")
+        self.pushButton_8.setStyleSheet("background-color: none")
+        self.pushButton_9.setStyleSheet("background-color: none")
+        self.lineEdit_2.setText(" ")
+        self.lineEdit.setText("turn: 0")
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
